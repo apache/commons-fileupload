eeab1805de13dbdee30c024b95aa76404c4574c0
Martin Cooper <martinc@apache.org>
2004-10-31 05:21:43 +0000

    Substantial refactoring and additions:
    * The core package is now independent of servlet / portlet / other
      distinctions, as well as persistence schemes, other than deprecated
      classes and methods retained for backwards compatibility.
    
    * Servlet specific functionality has been moved to a new 'servlet' package.
      Existing users should migrate to this as soon as possible, since the
      servlet specific functionality in the generic package will be removed in
      the release after FileUpload 1.1.
    
    * Support for portlets (JSR 168) has been added, in a new 'portlet'
      package. This is not well tested at this point, and feedback would be
      very much appreciated. (This also resolves bug #23620.)
    
    * The disk-based file item implementation has been moved into a 'disk'
      package, and renamed from Default* to Disk* to reflect what it really is.
      The Default* classes have been retained in the top level package for
      backwards compatibility, but are now deprecated, and will be removed in
      the release after FileUpload 1.1.
    
    * The isMultipartRequest method is an unfortunate casualty of this
      refactoring. That method should really be moved to ServletFileUpload, but
      since the method is static, it can only exist in either FileUploadBase or
      ServletFileUpload. Backwards compatibility dictates the former for now,
      but the latter is the desired state, which implies some future breakage.
      Fair warning...
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/fileupload/trunk@133399 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/fileupload/servlet/ServletRequestContext.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.fileupload.servlet;
+
+import java.io.InputStream;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.fileupload.RequestContext;
+
+/**
+ * <p>Provides access to the request information needed for a request made to
+ * an HTTP servlet.</p>
+ *
+ * @author <a href="mailto:martinc@apache.org">Martin Cooper</a>
+ *
+ * @since FileUpload 1.1
+ *
+ * @version $Id: ServletRequestContext.java,v 1.1 2004/10/31 05:21:43 martinc Exp $
+ */
+public class ServletRequestContext implements RequestContext {
+
+    // ----------------------------------------------------- Manifest constants
+
+    /**
+     * HTTP content type header name.
+     */
+    public static final String CONTENT_TYPE = "Content-type";
+
+
+    // ----------------------------------------------------- Instance Variables
+
+    /**
+     * The request for which the context is being provided.
+     */
+    private HttpServletRequest request;
+
+
+    // ----------------------------------------------------------- Constructors
+
+    /**
+     * Construct a context for this request.
+     *
+     * @param request The request to which this context applies.
+     */
+    public ServletRequestContext(HttpServletRequest request) {
+        this.request = request;
+    }
+
+
+    // --------------------------------------------------------- Public Methods
+
+    /**
+     * Retrieve the content type of the request.
+     *
+     * @return The content type of the request.
+     */
+    public String getContentType() {
+        return request.getHeader(CONTENT_TYPE);
+    }
+
+    /**
+     * Retrieve the content length of the request.
+     *
+     * @return The content length of the request.
+     */
+    public int getContentLength() {
+        return request.getContentLength();
+    }
+
+    /**
+     * Retrieve the input stream for the request.
+     *
+     * @return The input stream for the request.
+     *
+     * @throws IOException if a problem occurs.
+     */
+    public InputStream getInputStream() throws IOException {
+        return request.getInputStream();
+    }
+}
