b96f72f7af89fc09293936e6a0d04690b92e2478
Jochen Wiedmann <jochen@apache.org>
2010-07-13 09:12:53 +0000

    Rewrote comment, due to user feedback.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@963625 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/fileupload/InvalidFileNameException.java
+++ src/java/org/apache/commons/fileupload/InvalidFileNameException.java
@@ -21,11 +21,11 @@ package org.apache.commons.fileupload;
  * This exception is thrown in case of an invalid file name.
  * A file name is invalid, if it contains a NUL character.
  * Attackers might use this to circumvent security checks:
- * For example, the user might check, whether the file name
- * is "foo.exe\0.png". This file name might pass security
- * checks. OTOH, depending on the underlying C library, it
- * might create a file named "foo.exe", as the NUL character
- * is the string terminator in C.
+ * For example, a malicious user might upload a file with the name
+ * "foo.exe\0.png". This file name might pass security checks (i.e.
+ * checks for the extension ".png"), while, depending on the underlying
+ * C library, it might create a file named "foo.exe", as the NUL
+ * character is the string terminator in C.
  */
 public class InvalidFileNameException extends RuntimeException {
     private static final long serialVersionUID = 7922042602454350470L;
