8176c0d8a0b492007fc74a4195a763c8d16c5747
Mark Emlyn David Thomas <markt@apache.org>
2010-08-03 09:22:36 +0000

    Change Javadoc reference to non-deprecated method - Founf another one
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@981786 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/fileupload/FileUploadBase.java
+++ src/java/org/apache/commons/fileupload/FileUploadBase.java
@@ -43,7 +43,7 @@ import org.apache.commons.fileupload.util.Streams;
  * <p>This class handles multiple files per single HTML widget, sent using
  * <code>multipart/mixed</code> encoding type, as specified by
  * <a href="http://www.ietf.org/rfc/rfc1867.txt">RFC 1867</a>.  Use {@link
- * #parseRequest(HttpServletRequest)} to acquire a list of {@link
+ * #parseRequest(RequestContext)} to acquire a list of {@link
  * org.apache.commons.fileupload.FileItem}s associated with a given HTML
  * widget.</p>
  *
