1bec88867195be2337583e84ad1f0f703bb2b72b
Simone Tripodi <simonetripodi@apache.org>
2013-03-12 12:55:16 +0000

    [FILEUPLOAD-210] #comment as requested by the user, added a new method that maps each FileItem to its related field name, so users can access to them by key #resolved
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@1455515 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/fileupload/FileUploadBase.java
+++ src/main/java/org/apache/commons/fileupload/FileUploadBase.java
@@ -372,6 +372,39 @@ public abstract class FileUploadBase {
         }
     }
 
+    /**
+     * Processes an <a href="http://www.ietf.org/rfc/rfc1867.txt">RFC 1867</a>
+     * compliant <code>multipart/form-data</code> stream.
+     *
+     * @param ctx The context for the request to be parsed.
+     *
+     * @return A map of <code>FileItem</code> instances parsed from the request.
+     *
+     * @throws FileUploadException if there are problems reading/parsing
+     *                             the request or storing files.
+     *
+     * @since 1.3
+     */
+    public Map<String, List<FileItem>> parseParameterMap(RequestContext ctx)
+            throws FileUploadException {
+        final List<FileItem> items = parseRequest(ctx);
+        final Map<String, List<FileItem>> itemsMap = new HashMap<String, List<FileItem>>(items.size());
+
+        for (FileItem fileItem : items) {
+            String fieldName = fileItem.getFieldName();
+            List<FileItem> mappedItems = itemsMap.get(fieldName);
+
+            if (mappedItems == null) {
+                mappedItems = new ArrayList<FileItem>();
+                itemsMap.put(fieldName, mappedItems);
+            }
+
+            mappedItems.add(fileItem);
+        }
+
+        return itemsMap;
+    }
+
     // ------------------------------------------------------ Protected methods
 
     /**
