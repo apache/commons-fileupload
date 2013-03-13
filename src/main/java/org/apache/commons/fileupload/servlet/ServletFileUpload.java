4b23e68eeb18a2e560633c48a08a05520991fb60
Simone Tripodi <simonetripodi@apache.org>
2013-03-13 14:14:44 +0000

    [FILEUPLOAD-229]from RFC2616, section 19.3: "Although this document specifies the requirements for the generation of HTTP/1.1 messages, not all applications will be correct in their implementation. We therefore recommend that operational applications be tolerant of deviations whenever those deviations can be interpreted unambiguously."
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@1455949 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/fileupload/servlet/ServletFileUpload.java
+++ src/main/java/org/apache/commons/fileupload/servlet/ServletFileUpload.java
@@ -65,7 +65,7 @@ public class ServletFileUpload extends FileUpload {
      */
     public static final boolean isMultipartContent(
             HttpServletRequest request) {
-        if (!POST_METHOD.equals(request.getMethod())) {
+        if (!POST_METHOD.equalsIgnoreCase(request.getMethod())) {
             return false;
         }
         return FileUploadBase.isMultipartContent(new ServletRequestContext(request));
