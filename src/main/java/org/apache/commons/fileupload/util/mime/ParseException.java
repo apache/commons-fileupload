4c5f2ed0376972edbba319ead17f428f9fdd9a0c
Simone Tripodi <simonetripodi@apache.org>
2013-03-15 09:19:35 +0000

    checkstyle: Missing a Javadoc comment.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@1456824 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/fileupload/util/mime/ParseException.java
+++ src/main/java/org/apache/commons/fileupload/util/mime/ParseException.java
@@ -22,10 +22,15 @@ package org.apache.commons.fileupload.util.mime;
 public final class ParseException extends Exception {
 
     /**
-     *
+     * The UID to use when serializing this instance.
      */
     private static final long serialVersionUID = 5355281266579392077L;
 
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param message the detail message.
+     */
     public ParseException(String message) {
         super(message);
     }
