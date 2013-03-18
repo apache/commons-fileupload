7b2216c2766ab59b4a9f83c57c98ffc5f55232ad
Simone Tripodi <simonetripodi@apache.org>
2013-03-18 16:51:05 +0000

    [FILEUPLOAD-232] mime test class relocated in the right util.mime package
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@1457859 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/fileupload/util/mime/MimeUtilityTestCase.java
+++ src/test/java/org/apache/commons/fileupload/util/mime/MimeUtilityTestCase.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.fileupload.utils.mime;
+package org.apache.commons.fileupload.util.mime;
 
 import static org.junit.Assert.assertEquals;
 
