5c25d5d1b4eea4ede1b81ce41eea1c5abae0c374
Sebastian Bazley <sebb@apache.org>
2013-03-19 19:31:38 +0000

    Make exception messages more specific
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@1458467 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/fileupload/util/mime/QuotedPrintableDecoder.java
+++ src/main/java/org/apache/commons/fileupload/util/mime/QuotedPrintableDecoder.java
@@ -84,7 +84,7 @@ final class QuotedPrintableDecoder {
                 // we found an encoded character.  Reduce the 3 char sequence to one.
                 // but first, make sure we have two characters to work with.
                 if (off + 1 >= endOffset) {
-                    throw new IOException("Invalid quoted printable encoding");
+                    throw new IOException("Invalid quoted printable encoding; truncated escape sequence");
                 }
                 // convert the two bytes back from hex.
                 byte b1 = data[off++];
@@ -93,7 +93,7 @@ final class QuotedPrintableDecoder {
                 // we've found an encoded carriage return.  The next char needs to be a newline
                 if (b1 == '\r') {
                     if (b2 != '\n') {
-                        throw new IOException("Invalid quoted printable encoding");
+                        throw new IOException("Invalid quoted printable encoding; CR must be followed by LF");
                     }
                     // this was a soft linebreak inserted by the encoding.  We just toss this away
                     // on decode.
