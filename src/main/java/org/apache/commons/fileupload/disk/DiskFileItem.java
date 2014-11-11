293c6f76e03a26cf360a7893db3a92739e54f781
Thomas Neidhart <tn@apache.org>
2014-11-11 20:27:03 +0000

    Fix typo.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/fileupload/trunk@1638358 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/fileupload/disk/DiskFileItem.java
+++ src/main/java/org/apache/commons/fileupload/disk/DiskFileItem.java
@@ -488,7 +488,7 @@ public class DiskFileItem
      * be used for storing the contents of the file.
      *
      * @return An {@link java.io.OutputStream OutputStream} that can be used
-     *         for storing the contensts of the file.
+     *         for storing the contents of the file.
      *
      * @throws IOException if an error occurs.
      */
