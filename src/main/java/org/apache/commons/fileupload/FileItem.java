85551c6d55b24a40841d34e569b6dd4058309bb6
Gary Gregory <ggregory@apache.org>
2016-08-01 15:25:30 -0700

    The import java.io.Serializable is never used.

--- src/main/java/org/apache/commons/fileupload/FileItem.java
+++ src/main/java/org/apache/commons/fileupload/FileItem.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 
 /**
