b9f1f0b876a1544dfac135303258bc1a6aabbaf5
Gary Gregory <garydgregory@gmail.com>
2022-08-29 07:12:32 -0400

    Javadoc @see tags do not need to use a FQCN for classes in java.lang

--- src/main/java/org/apache/commons/fileupload2/disk/DiskFileItem.java
+++ src/main/java/org/apache/commons/fileupload2/disk/DiskFileItem.java
@@ -434,7 +434,7 @@ public class DiskFileItem
      *
      * @return The name of the form field.
      *
-     * @see #setFieldName(java.lang.String)
+     * @see #setFieldName(String)
      *
      */
     @Override
