f82ce643578c7a6ab27a009c4f6cf2bda2b5a422
Aimanfaiz1999 <aimenfaiz4444@gmail.com>
2023-05-22 01:45:46 +0200

    Removed the public scope for tests

--- src/test/java/org/apache/commons/fileupload2/util/mime/MimeUtilityTestCase.java
+++ src/test/java/org/apache/commons/fileupload2/util/mime/MimeUtilityTestCase.java
@@ -36,36 +36,36 @@ public final class MimeUtilityTestCase {
     }
 
     @Test
-    public void decodeInvalidEncoding() {
+    void decodeInvalidEncoding() {
         assertThrows(UnsupportedEncodingException.class, () -> MimeUtility.decodeText("=?invalid?B?xyz-?="));
     }
 
     @Test
-    public void decodeIso88591Base64Encoded() throws Exception {
+    void decodeIso88591Base64Encoded() throws Exception {
         assertEncoded("If you can read this you understand the example.",
                       "=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?= "
                             + "=?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=\"\r\n");
     }
 
     @Test
-    public void decodeIso88591Base64EncodedWithWhiteSpace() throws Exception {
+    void decodeIso88591Base64EncodedWithWhiteSpace() throws Exception {
         assertEncoded("If you can read this you understand the example.",
                 "=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?=\t  \r\n   =?ISO-8859-"
                       + "2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=\"\r\n");
     }
 
     @Test
-    public void decodeUtf8Base64Encoded() throws Exception {
+    void decodeUtf8Base64Encoded() throws Exception {
         assertEncoded(" h\u00e9! \u00e0\u00e8\u00f4u !!!", "=?UTF-8?B?IGjDqSEgw6DDqMO0dSAhISE=?=");
     }
 
     @Test
-    public void decodeUtf8QuotedPrintableEncoded() throws Exception {
+    void decodeUtf8QuotedPrintableEncoded() throws Exception {
         assertEncoded(" h\u00e9! \u00e0\u00e8\u00f4u !!!", "=?UTF-8?Q?_h=C3=A9!_=C3=A0=C3=A8=C3=B4u_!!!?=");
     }
 
     @Test
-    public void noNeedToDecode() throws Exception {
+    void noNeedToDecode() throws Exception {
         assertEncoded("abc", "abc");
     }
 }
