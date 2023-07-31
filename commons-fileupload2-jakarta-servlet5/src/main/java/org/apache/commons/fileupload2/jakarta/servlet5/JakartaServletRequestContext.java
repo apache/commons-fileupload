33a8b5533d03c7a306e0ea82fca6881c8b551071
Gary Gregory <garydgregory@gmail.com>
2023-07-31 12:27:31 -0400

    Refactor to support Jakarta Servlet 5 and 6

---
+++ commons-fileupload2-jakarta-servlet5/src/main/java/org/apache/commons/fileupload2/jakarta/servlet5/JakartaServletRequestContext.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.fileupload2.jakarta.servlet5;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.fileupload2.core.AbstractRequestContext;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * Provides access to the request information needed for a request made to an HTTP servlet.
+ */
+public class JakartaServletRequestContext extends AbstractRequestContext<HttpServletRequest> {
+
+    /**
+     * The request for which the context is being provided.
+     */
+    private final HttpServletRequest request;
+
+    /**
+     * Constructs a context for this request.
+     *
+     * @param request The request to which this context applies.
+     */
+    public JakartaServletRequestContext(final HttpServletRequest request) {
+        super(request::getHeader, request::getContentLength, request);
+        this.request = request;
+    }
+
+    /**
+     * Gets the character encoding for the request.
+     *
+     * @return The character encoding for the request.
+     */
+    @Override
+    public String getCharacterEncoding() {
+        return request.getCharacterEncoding();
+    }
+
+    /**
+     * Gets the content type of the request.
+     *
+     * @return The content type of the request.
+     */
+    @Override
+    public String getContentType() {
+        return request.getContentType();
+    }
+
+    /**
+     * Gets the input stream for the request.
+     *
+     * @return The input stream for the request.
+     * @throws IOException if a problem occurs.
+     */
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return request.getInputStream();
+    }
+
+}
