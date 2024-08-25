c68285505f43602eb2bbed073df1758b47f4c90d
Gary Gregory <garydgregory@gmail.com>
2024-08-25 12:09:33 -0400

    Let JUnit handle IOException

--- src/test/java/org/apache/commons/fileupload/DefaultFileItemTest.java
+++ src/test/java/org/apache/commons/fileupload/DefaultFileItemTest.java
@@ -21,7 +21,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
