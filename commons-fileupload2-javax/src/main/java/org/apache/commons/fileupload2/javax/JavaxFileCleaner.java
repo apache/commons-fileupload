b047526cf9ee6fa152612acf20ba343b115436ee
Gary D. Gregory <garydgregory@gmail.com>
2025-05-04 19:02:00 -0400

    Sort members

--- commons-fileupload2-javax/src/main/java/org/apache/commons/fileupload2/javax/JavaxFileCleaner.java
+++ commons-fileupload2-javax/src/main/java/org/apache/commons/fileupload2/javax/JavaxFileCleaner.java
@@ -43,13 +43,6 @@ public class JavaxFileCleaner implements ServletContextListener {
     }
 
     /**
-     * Constructs a new instance.
-     */
-    public JavaxFileCleaner() {
-        // empty
-    }
-
-    /**
      * Sets the instance of {@link FileCleaningTracker}, which is associated with the given {@link ServletContext}.
      *
      * @param servletContext The servlet context to modify
@@ -57,6 +50,13 @@ public class JavaxFileCleaner implements ServletContextListener {
      */
     public static void setFileCleaningTracker(final ServletContext servletContext, final FileCleaningTracker tracker) {
         servletContext.setAttribute(FILE_CLEANING_TRACKER_ATTRIBUTE, tracker);
+    }
+
+    /**
+     * Constructs a new instance.
+     */
+    public JavaxFileCleaner() {
+        // empty
     }
 
     /**
