23b9a7aba4ec39139837f796ac0c533bf4dd6618
Gary D. Gregory <garydgregory@gmail.com>
2025-05-04 18:21:44 -0400

    Javadoc

--- commons-fileupload2-core/src/main/java/org/apache/commons/fileupload2/core/FileItemFactory.java
+++ commons-fileupload2-core/src/main/java/org/apache/commons/fileupload2/core/FileItemFactory.java
@@ -38,6 +38,11 @@ public interface FileItemFactory<I extends FileItem<I>> {
      */
     abstract class AbstractFileItemBuilder<I extends FileItem<I>, B extends AbstractFileItemBuilder<I, B>> extends AbstractStreamBuilder<I, B> {
 
+        /**
+         * Create a new FileItemHeaders implementation.
+         *
+         * @return a new FileItemHeaders implementation.
+         */
         public static FileItemHeaders newFileItemHeaders() {
             return new FileItemHeadersImpl();
         }
@@ -75,60 +80,129 @@ public interface FileItemFactory<I extends FileItem<I>> {
          */
         private FileCleaningTracker fileCleaningTracker;
 
+        /**
+         * Constructs a new instance.
+         */
         public AbstractFileItemBuilder() {
             setBufferSize(DiskFileItemFactory.DEFAULT_THRESHOLD);
             setPath(PathUtils.getTempDirectory());
         }
 
+        /**
+         * Gets the content type.
+         *
+         * @return the content type.
+         */
         public String getContentType() {
             return contentType;
         }
 
+        /**
+         * Gets the field name.
+         *
+         * @return the field name.
+         */
         public String getFieldName() {
             return fieldName;
         }
 
+        /**
+         * Gets the file cleaning tracker.
+         *
+         * @return the file cleaning tracker.
+         */
         public FileCleaningTracker getFileCleaningTracker() {
             return fileCleaningTracker;
         }
 
+        /**
+         * Gets the field item headers.
+         *
+         * @return the field item headers.
+         */
         public FileItemHeaders getFileItemHeaders() {
             return fileItemHeaders;
         }
 
+        /**
+         * Gets the file name.
+         *
+         * @return the file name.
+         */
         public String getFileName() {
             return fileName;
         }
 
+        /**
+         * Tests whether this is a form field.
+         *
+         * @return whether this is a form field.
+         */
         public boolean isFormField() {
             return isFormField;
         }
 
+        /**
+         * Sets the content type.
+         *
+         * @param contentType the content type.
+         * @return {@code this} instance.
+         */
         public B setContentType(final String contentType) {
             this.contentType = contentType;
             return asThis();
         }
 
+        /**
+         * Sets the field name.
+         *
+         * @param fieldName the field name.
+         * @return {@code this} instance.
+         */
         public B setFieldName(final String fieldName) {
             this.fieldName = fieldName;
             return asThis();
         }
 
+        /**
+         * Sets the file cleaning tracker.
+         *
+         * @param fileCleaningTracker the file cleaning tracker.
+         * @return {@code this} instance.
+         */
         public B setFileCleaningTracker(final FileCleaningTracker fileCleaningTracker) {
             this.fileCleaningTracker = fileCleaningTracker;
             return asThis();
         }
 
+        /**
+         * Sets the file item headers.
+         *
+         * @param fileItemHeaders the item headers.
+         * @return {@code this} instance.
+         */
         public B setFileItemHeaders(final FileItemHeaders fileItemHeaders) {
             this.fileItemHeaders = fileItemHeaders != null ? fileItemHeaders : newFileItemHeaders();
             return asThis();
         }
 
+        /**
+         * Sets the file name.
+         *
+         * @param fileName the file name.
+         * @return {@code this} instance.
+         */
         public B setFileName(final String fileName) {
             this.fileName = fileName;
             return asThis();
         }
 
+        /**
+         * Sets whether this is a form field.
+         *
+         * @param isFormField whether this is a form field.
+         * @return {@code this} instance.
+         */
         public B setFormField(final boolean isFormField) {
             this.isFormField = isFormField;
             return asThis();
