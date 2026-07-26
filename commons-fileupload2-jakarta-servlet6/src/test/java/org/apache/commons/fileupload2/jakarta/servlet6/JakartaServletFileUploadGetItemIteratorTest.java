01f472aebcb18c724cee8a884c297d780001eecc
Gary Gregory <garydgregory@gmail.com>
2026-07-26 09:06:49 -0400

    Refactor tests to use @ParameterizedTest

--- commons-fileupload2-jakarta-servlet6/src/test/java/org/apache/commons/fileupload2/jakarta/servlet6/JakartaServletFileUploadGetItemIteratorTest.java
+++ commons-fileupload2-jakarta-servlet6/src/test/java/org/apache/commons/fileupload2/jakarta/servlet6/JakartaServletFileUploadGetItemIteratorTest.java
@@ -34,6 +34,8 @@ import org.apache.commons.fileupload2.core.FileUploadByteCountLimitException;
 import org.apache.commons.fileupload2.core.FileUploadException;
 import org.apache.commons.fileupload2.core.FileUploadFileCountLimitException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -129,11 +131,11 @@ class JakartaServletFileUploadGetItemIteratorTest {
 
     /**
      * When the number of uploaded files exceeds {@code maxFileCount}, iterating must throw a {@link FileUploadFileCountLimitException} with the expected
-     * permitted count.
+     * permitted count. Tested for maxFileCount values of 1, 2, 4, and 8.
      */
-    @Test
-    void testFileCountLimitExceededThrowsException() throws Exception {
-        final long maxFileCount = 2;
+    @ParameterizedTest
+    @ValueSource(longs = { 1, 2, 4, 8 })
+    void testFileCountLimitExceededThrowsException(final long maxFileCount) throws Exception {
         final var body = buildMultiFileParts((int) maxFileCount + 1);
         final HttpServletRequest request = new JakartaMockHttpServletRequest(body, CONTENT_TYPE);
         final var upload = newUpload();
@@ -147,21 +149,6 @@ class JakartaServletFileUploadGetItemIteratorTest {
         // The next hasNext() call triggers the limit check
         final var ex = assertThrows(FileUploadFileCountLimitException.class, iter::hasNext);
         assertEquals(maxFileCount, ex.getPermitted());
-    }
-
-    /**
-     * A file-count limit of 1 allows exactly one file; attempting to move to the second must throw {@link FileUploadFileCountLimitException}.
-     */
-    @Test
-    void testFileCountLimitOfOneThrowsOnSecondFile() throws Exception {
-        final var body = buildMultiFileParts(2);
-        final HttpServletRequest request = new JakartaMockHttpServletRequest(body, CONTENT_TYPE);
-        final var upload = newUpload();
-        upload.setMaxFileCount(1L);
-        final FileItemInputIterator iter = upload.getItemIterator(request);
-        assertTrue(iter.hasNext());
-        iter.next(); // consume the first (allowed) item
-        assertThrows(FileUploadFileCountLimitException.class, iter::hasNext);
     }
 
     /**
